e83c041dcd60637e01b8f62b19519a3eac24b512
romancuscenco <r.cuscenco@yandex.ru>
2014-01-03 18:35:09 +0200

    Started CMS

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,46 @@
-﻿var object = {};
+﻿var App = {
+    Models: {},
+    Collections: {},
+    Contacts: null
+};
 
-_.extend(object, Backbone.Events);
+$(function () { //Run this code when the DOM is ready
 
-object.on("show-message", function (msg) {
-    $("#display .listing").text(msg);
+    window.tom = new App.Models.Contact({
+        firstName: 'Thomas',
+        lastName: 'Hunter',
+        phoneNumber: '987654321',
+        email: 'me@thomashunter.name'
+    });
+
+    App.Contacts = new App.Collections.Contact();
+
+    App.Contacts.add(window.tom);
+
+    App.Contacts.add({
+        firstName: 'Rupert',
+        lastName: 'Styx',
+        phoneNumber: '123456789',
+        email: 'rupertstyx@example.net'
+    });
+
+    App.Contacts.add({});
+
+    var contactListing = '';
+
+    App.Contacts.each(function (contact) {
+        contactListing += "<div>" +
+            contact.get('firstName') + " " +
+            contact.get('lastName') + " ";
+
+        if (contact.isValid()) {
+            contactListing += "(valid)";
+        } else {
+            contactListing += "(invalid)";
+        }
+
+        contactListing += "</div>";
+    });
+
+    $("#display").html(contactListing);
 });
-
-object.trigger("show-message", "Hello World");
