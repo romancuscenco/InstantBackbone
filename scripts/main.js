e58717f00859e60d17d5a8907fd2029b48a6ad42
romancuscenco <r.cuscenco@yandex.ru>
2014-01-03 17:27:48 +0200

    First commit and Hello World

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+﻿var object = {};
+
+_.extend(object, Backbone.Events);
+
+object.on("show-message", function (msg) {
+    $("#display .listing").text(msg);
+});
+
+object.trigger("show-message", "Hello World");
